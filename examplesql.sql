de7b347c4992314f03d10d564902d9801efbaf65
mooner92 <chlaudgjs0989@gmail.com>
2024-05-07 20:08:46 +0900

    add

--- examplesql.sql
+++ examplesql.sql
@@ -30,6 +30,13 @@ CREATE TABLE shows (
 
 INSERT INTO users (username,useremail,passwd,created_at) VALUES (
     '박빙구','bing999@hanmail.net','qwer1234!!','2017-02-13 09:36:25');
-SELECT * username FROM users While username = 'qwer1234!!';
+SELECT username FROM users WHERE username = 'qwer1234!!';
 
-INSERT INTO shows(showname,show_date,show_price,show_info,show_image_path) VALUES ()
+
+INSERT INTO shows(showname,show_date,show_price,show_info,show_image_path) VALUES (
+    '위대한쇼맨','2017-03-21',8000,'마이클베이 감독의 유쾌상쾌통쾌 액션. . .','./src/images/show_images/the_greatest_showman_poster.jpeg');
+SELECT show_price FROM shows WHERE showname = '위대한쇼맨';
+
+INSERT INTO orders(ordernum,ordered_at,showname,ordered_username,ordered_useremail,show_date,ordered_quantity,show_price,total_ordered_price) VALUES (
+    217,'2017-03-17','위대한쇼맨','박빙구','bing999@hanmail.net','2017-03-21',186,8000,795000);
+SELECT * FROM orders WHERE ordernum = 217;
