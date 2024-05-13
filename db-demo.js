4c300cc9b95464562527bfd85abf2a9221538d03
mooner92 <chlaudgjs0989@gmail.com>
2024-05-13 23:02:47 +0900

    add 0509

--- db-demo.js
+++ db-demo.js
@@ -3,7 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
-    password : 'mypassword',
+    password : 'mypassword', 
+    timezone : 'Asia/Seoul',
     database : 'Youtube'
 });
 
